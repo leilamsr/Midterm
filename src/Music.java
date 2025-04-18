2403062d87cfde85f853db4fd11194ac79d4198d
leila <leila.m2006131@gmail.com>
2025-04-18 15:12:00 +0330

    add UserBehavior & Music & InvalidOperationException

--- src/Music.java
+++ src/Music.java
@@ -1,2 +1,55 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Music {
+    private String title;
+    private User singer;
+    private int numberOfStream = 0;
+    public static ArrayList<Music> allMusics;
+
+    public Music(String title, User singer) {
+        this.title = title;
+        this.singer = singer;
+        allMusics.add(this);
+    }
+
+    public void play() {
+        System.out.println("اهنگ " + title + " با خواننده " + singer + "در حال پخش است.");
+        numberOfStream++;
+    }
+
+    public static List<Music> Search(String title) {
+        List<Music> musics = new ArrayList<>();
+        for (Music music : allMusics) {
+            if (music.title.equals(title)) {
+                musics.add(music);
+            }
+        }
+        if (musics.isEmpty()) {
+            return null;
+        }
+        return musics;
+    }
+
+    public static Music Search(String title, String singer) {
+        for (Music music : allMusics) {
+            if (music.title.equals(title) && music.singer.equals(singer)) {
+                return music;
+            }
+        }
+        return null;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSinger() {
+        return singer.toString();
+    }
+
+    public int getNumberOfStream() {
+        return numberOfStream;
+    }
+
 }
--- src/RegularBehavior.java
+++ src/RegularBehavior.java
@@ -1,2 +1,20 @@
-public class RegularBehavior implements UserBehavior{
+public class RegularBehavior implements UserBehavior {
+    private int playingLimit = 5;
+
+    @Override
+    public void createPlaylist (String Title, User Owner) {
+        throw new InvalidOperationException("کاربر عادی امکان ایجاد پلی لیست را ندارد!");
+    }
+
+    @Override
+    public void playMusic (Music music) {
+        if (playingLimit <= 0) {
+            throw new InvalidOperationException("کاربر گرامی به حد مجاز استفاده روزانه خود رسیدهد اید!");
+        }
+        music.play();
+        playingLimit--;
+    }
+
+    @Override
+    public void buyPremium (User owner, int month) {}
 }
